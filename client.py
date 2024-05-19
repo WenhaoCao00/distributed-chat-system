1d667ed54359581bb5f5d1ac5c63d14d0b937f1b
Wenhao Cao <hankcaowenhao@gmail.com>
2024-05-19 21:38:45 +0200

    ok for multi client

--- client.py
+++ client.py
@@ -2,37 +2,51 @@ import socket
 import threading
 import os
 import time
+import uuid
 from service_discovery import ServiceDiscovery
+from lamport_clock import LamportClock  # 假设你将LamportClock类保存到这个文件中
 
 def receive_messages(client_socket):
     while True:
         try:
             data, server = client_socket.recvfrom(1024)
-            print(f'Received message: {data.decode()}')
+            message = data.decode()
+            if message.startswith("REJECTED"):
+                print("Name already taken. Please enter a different name.")
+                return False
+            elif message.startswith("ACCEPTED"):
+                print("Name accepted. You can start sending messages.")
+                return True
+            else:
+                print(f'Received message: {message}')
         except OSError as e:
             print(f"Error receiving data: {e}")
             break
 
-def send_message(server_address, server_port):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.bind(('0.0.0.0', 0))  # 绑定到一个临时端口
+def register_name(client_socket, server_address, server_port):
+    while True:
+        name = input("Enter your name: ")
+        register_message = f'REGISTER:{name}'
+        client_socket.sendto(register_message.encode(), (server_address, server_port))
+        if receive_messages(client_socket):
+            return name
 
-    # 启动接收消息的线程
-    receiver_thread = threading.Thread(target=receive_messages, args=(client_socket,), daemon=True)
-    receiver_thread.start()
-
+def send_messages(client_socket, server_address, server_port, clock, client_name):
     try:
         while True:
             message = input("Enter message to send: ")
-            client_socket.sendto(message.encode(), (server_address, server_port))
-            print(f'Sent to server: {message}')
+            if message.strip().lower() == "exit":
+                print("Exiting...")
+                break
+            clock.increment()
+            message_id = str(uuid.uuid4())  # 生成唯一消息ID
+            full_message = f'CLIENT:{message_id}:{clock.get_time()}:{client_name}:{message}'
+            client_socket.sendto(full_message.encode(), (server_address, server_port))
+            print(f'Sent to server: {full_message}')
     except KeyboardInterrupt:
         print("Client is closing.")
-    finally:
-        client_socket.close()
-        print('Socket closed')
 
-if __name__ == '__main__':
+def main():
     service_discovery = ServiceDiscovery()
     service_discovery.start()
 
@@ -45,6 +59,28 @@ if __name__ == '__main__':
 
     if not server_addresses:
         print("No servers discovered, exiting.")
-    else:
-        server_address = server_addresses[0]  # 连接到第一个发现的服务器
-        send_message(server_address, 10001)
+        return
+
+    clock = LamportClock()
+    server_address = server_addresses[0]  # 连接到第一个发现的服务器
+    server_port = 10001
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    client_socket.bind(('0.0.0.0', 0))  # 绑定到一个临时端口
+
+    # 注册名字
+    client_name = register_name(client_socket, server_address, server_port)
+
+    # 启动接收消息的线程
+    receiver_thread = threading.Thread(target=receive_messages, args=(client_socket,), daemon=True)
+    receiver_thread.start()
+
+    # 启动发送消息的逻辑
+    send_messages(client_socket, server_address, server_port, clock, client_name)
+
+    # 关闭socket
+    client_socket.close()
+    print('Socket closed')
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -4,31 +4,68 @@ import multiprocessing
 import os
 import time
 from service_discovery import ServiceDiscovery
+from lamport_clock import LamportClock  # 假设你将LamportClock类保存到这个文件中
 
 class ChatServer(multiprocessing.Process):
-    def __init__(self, server_socket, received_data, client_address, connected_clients, server_addresses):
+    def __init__(self, server_socket, received_data, client_address, connected_clients, client_names, server_addresses, processed_messages, clock):
         super(ChatServer, self).__init__()
         self.server_socket = server_socket
         self.received_data = received_data
         self.client_address = client_address
         self.connected_clients = connected_clients
+        self.client_names = client_names
         self.server_addresses = server_addresses
+        self.processed_messages = processed_messages
+        self.clock = clock
 
     def run(self):
-        # Broadcast message to other servers
-        for server_address in self.server_addresses:
-            self.server_socket.sendto(self.received_data, (server_address, 10001))
+        data = self.received_data.decode()
+        parts = data.split(':', 3)
+        message_type = parts[0]
 
-        # Send message to all connected clients
-        for client in self.connected_clients:
-            self.server_socket.sendto(self.received_data, client)
+        if message_type == "REGISTER":
+            client_name = parts[1]
+            if client_name in self.client_names:
+                response = f'REJECTED:{client_name}'
+                self.server_socket.sendto(response.encode(), self.client_address)
+            else:
+                self.client_names[client_name] = self.client_address
+                response = f'ACCEPTED:{client_name}'
+                self.server_socket.sendto(response.encode(), self.client_address)
+                print(f'Client {client_name} has joined from {self.client_address}')
+                self.connected_clients.append((client_name, self.client_address))  # 添加到 connected_clients 列表
 
-        # Send acknowledgement to the original sender
-        message = f'Server {os.getpid()} received your message: {self.received_data.decode()}'
-        self.server_socket.sendto(message.encode(), self.client_address)
-        print(f'Sent to client {self.client_address}: {message}')
+        elif message_type == "CLIENT":
+            try:
+                message_id, received_time, client_name_and_message = parts[1], int(parts[2]), parts[3]
+                client_name, message = client_name_and_message.split(':', 1)
 
-def server_listener(server_socket, connected_clients, server_addresses):
+                if message_id in self.processed_messages:
+                    return
+
+                self.processed_messages.append(message_id)
+                self.clock.update(received_time)
+
+                # Broadcast message to all connected clients
+                broadcast_message = f'{client_name}: {message} (Lamport time: {self.clock.get_time()})'
+
+                print("Broadcasting to clients...")
+                print(self.connected_clients)
+                for client in self.connected_clients:
+                    self.server_socket.sendto(broadcast_message.encode(), client[1])
+
+                # Send acknowledgement to the original sender
+                ack_message = f'SERVER_ACK:{self.clock.get_time()}:{message}'
+                self.server_socket.sendto(ack_message.encode(), self.client_address)
+                print(f'Sent to client {self.client_address}: {ack_message}')
+            except ValueError as e:
+                print(f"Error processing message: {data}, Error: {e}")
+
+        elif message_type == "SERVER_ACK":
+            # Process server acknowledgement message if necessary
+            pass
+
+def server_listener(server_socket, connected_clients, client_names, server_addresses, processed_messages, clock):
     buffer_size = 1024
     while True:
         data, address = server_socket.recvfrom(buffer_size)
@@ -38,9 +75,7 @@ def server_listener(server_socket, connected_clients, server_addresses):
             server_addresses.add(address[0])
         else:
             print(f'Received message \'{data.decode()}\' from {address}')
-            if address not in connected_clients:
-                connected_clients.append(address)
-            p = ChatServer(server_socket, data, address, connected_clients, server_addresses)
+            p = ChatServer(server_socket, data, address, connected_clients, client_names, server_addresses, processed_messages, clock)
             p.start()
             p.join()
 
@@ -61,5 +96,9 @@ if __name__ == "__main__":
     server_socket.bind(('0.0.0.0', 10001))  # 绑定到所有可用的网络接口
     print('Server up and running')
 
-    connected_clients = []
-    server_listener(server_socket, connected_clients, server_addresses)
+    manager = multiprocessing.Manager()
+    connected_clients = manager.list()
+    client_names = manager.dict()
+    processed_messages = manager.list()  # 使用列表代替集合
+    clock = LamportClock()
+    server_listener(server_socket, connected_clients, client_names, server_addresses, processed_messages, clock)
